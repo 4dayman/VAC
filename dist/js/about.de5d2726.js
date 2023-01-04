228241962949ce9f6b60f9b7e3e83797f56259f9
4dayman <109858025+4dayman@users.noreply.github.com>
2023-01-04 13:54:24 +0200

    commit

---
+++ dist/js/about.de5d2726.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvac"]=self["webpackChunkvac"]||[]).push([[443],{399:function(n,t,u){u.r(t),u.d(t,{default:function(){return r}});var s=function(){var n=this;n._self._c;return n._m(0)},a=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],e=u(1),c={},l=(0,e.Z)(c,s,a,!1,null,null,null),r=l.exports}}]);
+//# sourceMappingURL=about.de5d2726.js.map
